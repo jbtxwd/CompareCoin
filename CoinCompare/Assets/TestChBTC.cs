7ef80cac16103f3462c3e54c21c787841a06f156
jbtxwd <8796991@qq.com>
2017-07-12 07:51:09 +0800

    3

---
+++ CoinCompare/Assets/TestChBTC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using chbtc;
+using System;
+using Common;
+public class TestChBTC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("k"))
+        {
+            string s = chbtc_api.testGetAccountInfo();
+            CoroutineManager.instance.StartCoroutine(PostWWW(s, null));
+            Debug.Log(s);
+        }
+	}
+
+    IEnumerator PostWWW(string url, Action<WWW> result)
+    {
+        var www = new WWW(url);
+        yield return www;
+        if (string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.text);
+            if (result != null)
+                result(www);
+        }
+        else
+        {
+            Debug.Log("post error+" + www.error.ToString() + "==url==" + url);
+        }
+    }
+}
