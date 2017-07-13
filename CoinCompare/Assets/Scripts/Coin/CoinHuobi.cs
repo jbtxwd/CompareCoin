f6f29638dc1058340fb3df1e0934fcaa5a6d4333
jbtxwd <8796991@qq.com>
2017-07-13 23:30:05 +0800

    *

--- CoinCompare/Assets/Scripts/Coin/CoinHuobi.cs
+++ CoinCompare/Assets/Scripts/Coin/CoinHuobi.cs
@@ -7,6 +7,8 @@ using ICSharpCode.SharpZipLib;
 using ICSharpCode.SharpZipLib.GZip;
 using System.IO;
 using System.Text;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 public class SubModel
 {
     public SubModel(string _sub,long _id)
@@ -38,6 +40,31 @@ public class Pong
 	public long pong;
 }
 
+public class TickDetail
+{
+    public string ch;
+    public string ts;
+    public Tick tick;
+}
+
+public class Tick
+{
+    public List<List<float>> bids;
+    public List<List<float>> asks;
+}
+
+
+//public class Bids
+//{
+   
+//}
+
+//public class Asks
+//{
+   
+//}
+
+
 public class CoinHuobi
 {
 
@@ -57,6 +84,27 @@ public class CoinHuobi
         webSocketBE.OnOpen += OnWebSocketBEOpen;
         webSocketBE.OnBinary += OnBEBinaryMessageReceived;
         webSocketBE.Open();
+
+        //TickDetail _Td = new TickDetail();
+        //_Td.ch = "fuck";
+        //_Td.ts = "trtr";
+        //_Td.tick = new Tick();
+        //List<float> l1 = new List<float>();
+        //l1.Add(1);
+        //l1.Add(2);
+        //List<List<float>> l2 = new List<List<float>>();
+        //l2.Add(l1);
+        //_Td.tick.asks = l2;
+        //string final = JsonConvert.SerializeObject(_Td);
+        //Debug.Log(final);
+        //_Td.tick.asks = new float[][];
+        //TradeInfo ti = new TradeInfo();
+        //ti.count = 100;
+        //ti.price = 90.0f;
+        //_Td.tick.asks.Add(ti);
+        //_Td.tick.bids.Add(ti);
+        //string final = JsonConvert.SerializeObject(_Td);
+        //Debug.Log(final);
     }
 
     void OnWebSocketBEOpen(WebSocket _ws)
@@ -79,6 +127,12 @@ public class CoinHuobi
         }
         byte[] depress = re.ToArray();
         string _result = Encoding.UTF8.GetString(depress);
+        Debug.Log("time==" + Time.time + _result);
+        //string _final = "";
+        ///*     string _final = StringToJson(_result); */;
+
+        //Dictionary<string, object> _jsonData = JsonConvert.DeserializeObject(_final) as Dictionary<string, object>;
+        //Debug.Log(_jsonData.Count);
         if (_result.Contains("ping"))
         {
             Ping _ping = JsonUtility.FromJson<Ping>(_result);
@@ -89,7 +143,14 @@ public class CoinHuobi
         }
         else
         {
-            Debug.Log("time==" + Time.time + _result);
+            //TickDetail _Td = JsonConvert.DeserializeObject<TickDetail>(_result); // JsonUtility.FromJson<TickDetail>(_result);
+            TickDetail _Td = JsonUtility.FromJson<TickDetail>(_result);
+            if (!string.IsNullOrEmpty(_Td.ch))
+            {
+                Debug.Log(_Td.tick.asks[0][0]+"**"+ _Td.tick.asks[0][1]);
+                //Debug.Log(_Td.tick.asks[0][1]);
+            }
+            //Debug.Log("time==" + Time.time + _result);
         }
     }
 
