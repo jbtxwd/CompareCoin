e81acf7bda83ffbe010c1490ed6e7f306f3e8c21
Jason <8796991@qq.com>
2017-08-10 20:30:19 +0800

    *

---
+++ CoinCompare/Assets/Editor/PlatformSettingEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+public class PlatformSettingEditor 
+{
+    [MenuItem("Tool/PlatformSetting")]
+    // Use this for initialization
+    static void InitialPlatformSetting () 
+    {
+        List<PlatFormSetting> _psList = new List<PlatFormSetting>();
+
+        PlatFormSetting _ps19800 = new PlatFormSetting();
+        _ps19800.platformName = "19800";
+        _ps19800.key = "1";
+        _ps19800.security = "4";
+        _psList.Add(_ps19800);
+
+		PlatFormSetting _psjubi = new PlatFormSetting();
+		_psjubi.platformName = "jubi";
+		_psjubi.key = "1";
+		_psjubi.security = "4";
+		_psList.Add(_psjubi);
+
+        string _json= JsonConvert.SerializeObject(_psList);
+        Debug.Log(_json);
+        CommonTool.CreateFile(Application.persistentDataPath,"platformSettings.json",_json);
+        Debug.Log(CommonTool.ReadFile(Application.persistentDataPath,"platformSettings.json"));
+
+	}
+}
---
+++ CoinCompare/Assets/Scripts/CommonTool.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+public class CommonTool
+{
+    public static void CreateFile(string path, string name, string info)
+    {
+        //文件流信息
+        StreamWriter _sw;
+        FileInfo _t = new FileInfo(path + "//" + name);
+        if (!_t.Exists)
+        {
+            //如果此文件不存在则创建
+            _sw = _t.CreateText();
+        }
+        else
+        {
+            //如果此文件存在则打开
+            _sw = _t.AppendText();
+        }
+        //以行的形式写入信息
+        _sw.WriteLine(info);
+        //关闭流
+        _sw.Close();
+        //销毁流
+        _sw.Dispose();
+    }
+
+    public static string ReadFile(string _path, string _name)
+    {
+        //使用流的形式读取
+        StreamReader _sr = null;
+        try
+        {
+            _sr = File.OpenText(_path + "//" + _name);
+        }
+        catch
+        {
+            //路径与名称未找到文件则直接返回空
+            return null;
+        }
+        string _content = _sr.ReadToEnd();
+
+        //关闭流
+        _sr.Close();
+        //销毁流
+        _sr.Dispose();
+        //将数组链表容器返回
+        return _content;
+    }
+}
