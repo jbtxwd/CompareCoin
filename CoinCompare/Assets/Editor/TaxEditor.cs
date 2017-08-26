5a9eb2b96181b1d5a004e9e6384754b8bb0916ec
Jason <8796991@qq.com>
2017-08-26 15:01:07 +0800

    *

---
+++ CoinCompare/Assets/Editor/TaxEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+public class TaxEditor
+{
+    [MenuItem("Tool/TaxSetting")]
+    // Use this for initialization
+    static void Initial()
+    {
+        List<Tax> _psList = new List<Tax>();
+
+        Tax _tax = new Tax();
+        _tax.platformName = "19800";
+        _tax.coinName = Coins.BTC;
+        _tax.sellTax = 0.001f;
+        _tax.buyTax = 0.001f;
+        _psList.Add(_tax);
+
+        string _json = JsonConvert.SerializeObject(_psList);
+        Debug.Log(_json);
+        CommonTool.CreateFile(Application.persistentDataPath, "TaxSettings.json", _json);
+        Debug.Log(CommonTool.ReadFile(Application.persistentDataPath, "TaxSettings.json"));
+
+    }
+}
---
+++ CoinCompare/Assets/Scripts/Define/Platforms.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms 
+{
+    public const string p19800 = "19800";
+    public const string phuobi = "huobi";
+    public const string pchbtc = "chbtc";
+}
